c43d4f79069a9e97d736b6ab498d2e90064ea325
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-11-06 03:05:57 +0700

    first commit

---
+++ bin/learn_json_serialization.dart
@@ -0,0 +1,5 @@
+import 'package:learn_json_serialization/learn_json_serialization.dart' as learn_json_serialization;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_json_serialization.calculate()}!');
+}
---
+++ test/learn_json_serialization_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_json_serialization/learn_json_serialization.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
